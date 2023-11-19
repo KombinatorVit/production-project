e668d80a33be58a0f8dda20165c6dcce1c39eb36
KombinatorVit <slonik0775@gmail.com>
2023-11-19 14:54:51 +0200

    add jest + first unit test

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,12 +1,15 @@
-type Mods = Record<string, boolean | string>
+type Mods = Record<string, boolean | string>;
 
 export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
+    const additionalClasses = additional ? additional.filter(Boolean) : [];
+
     return [
         cls,
-        ...additional.filter(Boolean),
-        ...Object.entries(mods)
-            .filter(([className, value]) => Boolean(value))
-            .map(([className]) => className),
-    ]
-        .join(' ');
+        ...additionalClasses,
+        ...(mods
+            ? Object.entries(mods)
+                .filter(([classname, value]) => Boolean(value))
+                .map(([className]) => className)
+            : []),
+    ].join(' ');
 }
