ab4d8dba26f5c715e67fef1c38f62c4cf0a28755
KombinatorVit <slonik0775@gmail.com>
2023-11-19 13:52:56 +0200

    add eslint + webpack config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,26 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import './styles/index.scss';
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AppRouter} from "app/providers/route";
-import {Navbar} from "widgets/Navbar";
-import {Sidebar} from "widgets/Sidebar";
-
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/route';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 const App = () => {
-    const {theme} = useTheme();
-    
+    const { theme } = useTheme();
+
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
-                
-                <Navbar/>
-                <div className={'content-page'}>
-                    
-                    <Sidebar/>
-                    <div className={'page-wrapper'}><AppRouter/>
+
+                <Navbar />
+                <div className="content-page">
+
+                    <Sidebar />
+                    <div className="page-wrapper">
+                        <AppRouter />
                     </div>
-                
+
                 </div>
             </Suspense>
         </div>
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -2,7 +2,7 @@ declare module '*.scss' {
     interface IClassNames {
         [className: string]: string
     }
-    
+
     const classNames: IClassNames;
     export = classNames;
 }
@@ -12,7 +12,7 @@ declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg' {
     import React from 'react';
-    
+
     const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
     export default SVG;
 }
